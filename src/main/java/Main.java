e45f4f89bd8a8d6d3860c4cc2a5b21989eabb1bd
Chris <chris@Zipcoders-MacBook-Pro-4.local>
2022-07-02 17:55:00 -0400

    Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner userInput = new Scanner(System.in);
+        int input = 0;
+        boolean rerun = false;
+        while (!rerun) {
+            System.out.println("Input Number");
+            input = userInput.nextInt();
+            for (int i = input - 1; i >= 0; i--) {
+                input += i;
+            }
+            int result = input;
+            System.out.println(result);
+        }
+        rerun = true;
     }
+
+
+
+
+
+
 }
+
